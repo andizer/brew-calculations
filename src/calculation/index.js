0d6848a03f617661b88b07f48f2bf0375d9769d2
Andy <325040+andizer@users.noreply.github.com>
2021-07-09 17:24:57 +0200

    Expose the color scales in the index

--- src/calculation/index.js
+++ src/calculation/index.js
@@ -1,7 +1,7 @@
 
 export { calculateAlcoholPercentage } from "./alcoholPercentage";
 export { calculateAttenuation } from "./attenuation";
-export { convertColor } from "./convertColor";
+export { convertColor, SCALE_EBC, SCALE_SRM, SCALE_LOVIBOND } from "./convertColor";
 export { correctHydrometerTemperature } from "./correctHydrometerTemperature";
 export { calculateGravityCorrection } from "./gravityCorrection";
 
