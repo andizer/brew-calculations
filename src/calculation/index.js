6af399554eadf310d38bda2a05cb83a1994eb7ba
Andy <325040+andizer@users.noreply.github.com>
2021-07-09 17:32:03 +0200

    Adds the plato and gravity convert methods

--- src/calculation/index.js
+++ src/calculation/index.js
@@ -2,6 +2,8 @@
 export { calculateAlcoholPercentage } from "./alcoholPercentage";
 export { calculateAttenuation } from "./attenuation";
 export { convertColor, SCALE_EBC, SCALE_SRM, SCALE_LOVIBOND } from "./convertColor";
+export { convertGravityPlato } from "./convertGravityPlato";
+export { convertPlatoGravity } from "./convertPlatoGravity";
 export { correctHydrometerTemperature } from "./correctHydrometerTemperature";
 export { calculateGravityCorrection } from "./gravityCorrection";
 
